070a2342c231dda331048f61f4c190961b69854a
Mason Embry <mason@embrycode.com>
2020-12-16 08:07:03 -0600

    Change routing for /api

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,15 @@
 HOST=pi@67.177.189.251
 
 ssh $HOST << EOF
+  echo "Logged into host. Navigating to backend directory..."
   cd /var/www/dev.embrycode.com/backend
-  touch deleteMe
+
+  echo "Pulling from GitHub..."
+  git pull
+
+  echo "Installing dependencies..."
+  npm i
+
+  echo "Success. Exiting..."
   exit
 EOF
