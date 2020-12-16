48d459be722e6a4809c2ef64e834345dabd11038
Mason Embry <mason@embrycode.com>
2020-12-16 08:48:32 -0600

    Improve script and add pm2

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,19 @@
 HOST=pi@67.177.189.251
 
 ssh $HOST << EOF
-  echo "\nLogged into host. Navigating to backend directory..."
+  printf "\nLogged into host. Navigating to backend directory..."
   cd /var/www/dev.embrycode.com/backend
 
-  echo "\nPulling from GitHub..."
+  printf "\n\nPulling from GitHub...\n"
   git pull
 
-  echo "\nInstalling dependencies..."
+  printf "\nInstalling dependencies...\n"
   NODE_ENV=production
   npm i
 
-  echo "\nSuccess. Exiting..."
+  printf "\nStarting server...\n"
+  pm2 start npm --name "raspberry-pi-server-api" -- start
+
+  printf "\nSuccess. Exiting..."
   exit
 EOF
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function (req, res) {
-  res.json({ message: 'It worked!' })
+router.get('/health-check', function (req, res) {
+  res.json('It worked')
 })
 
 module.exports = router
